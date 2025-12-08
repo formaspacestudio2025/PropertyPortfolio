46fc9e174a493a2b13ea5f21b656d40a536e32af
formaspacestudio2025 <formaspacestudio@gmail.com>
2025-12-08 08:20:45 +0100

    Create menu.js

---
+++ assets/js/menu.js
@@ -0,0 +1,14 @@
+function toggleMenu(){
+  document.getElementById("sideMenu").classList.toggle("open");
+}
+
+function toggleDropdown(){
+  document.getElementById("portfolioMenu").classList.toggle("open");
+}
+
+/* Close after click */
+document.querySelectorAll('.side-menu a').forEach(link=>{
+  link.addEventListener('click',()=>{
+    document.getElementById("sideMenu").classList.remove("open");
+  });
+});
